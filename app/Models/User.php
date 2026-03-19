2008fb6b9559c7f51a2547baa9d943c27ccab0a7
Ondřej Musil <ondrej@musilondrej.com>
2026-03-19 14:27:18 +0100

    Implement FilamentUser interface and add canAccessPanel method to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory;
     use Notifiable;
@@ -52,6 +54,11 @@ class User extends Authenticatable
             'default_hourly_rate' => 'decimal:2',
             'dashboard_widgets' => 'array',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 
     protected static function booted(): void
